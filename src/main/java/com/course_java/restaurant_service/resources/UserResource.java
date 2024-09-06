a9512c449f41d3f488913a81de25b3e2472ab52d
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-09-06 15:24:28 -0300

    feat: create User insert

--- src/main/java/com/course_java/restaurant_service/resources/UserResource.java
+++ src/main/java/com/course_java/restaurant_service/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.course_java.restaurant_service.entities.User;
 import com.course_java.restaurant_service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController // Define a classe como um controlador REST
@@ -18,7 +17,7 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
-    @GetMapping // Responde a requisições HTTP GET
+    @GetMapping // Responde a requisições HTTP GET do RequestMapping
     public ResponseEntity<List<User>> findAll() {
         List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list); // Retorna o objeto User com status HTTP 200 (OK)
@@ -30,5 +29,14 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        //rota dinâmica
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
